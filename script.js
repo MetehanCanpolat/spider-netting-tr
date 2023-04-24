37a8e4e98b512fdc95ca88e1b4082a2b84d4845d
MetehanCanpolat <120008642+MetehanCanpolat@users.noreply.github.com>
2023-04-24 21:00:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openModal(img) {
+	
+	var modal = document.getElementById("myModal");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	modal.style.display = "block";
+	modalImg.src = img.src;
+	captionText.innerHTML = img.alt;
+}
+
+function closeModal() {
+
+	var modal = document.getElementById("myModal");
+	modal.style.display = "none";
+}
+
+
